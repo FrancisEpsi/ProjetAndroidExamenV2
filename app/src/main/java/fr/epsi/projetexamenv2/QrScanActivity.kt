3fecbad55d609bfe8b936592c1aaaf32e201e655
François Saura <franois.saura@gmail.com>
2022-03-02 14:52:58 +0100

    Add "QrScanActivity"

---
+++ app/src/main/java/fr/epsi/projetexamenv2/QrScanActivity.kt
@@ -0,0 +1,11 @@
+package fr.epsi.projetexamenv2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QrScanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_qr_scan)
+    }
+}
