58dd9496c0c825067a1c04e54945aed1881ff312
François Saura <franois.saura@gmail.com>
2022-03-02 20:52:32 +0100

    Prise en charge de la vérification de la permission de la caméra dès le lancement de l'activité QrScanACtivity et demande la permission si l'application n'est pas autorisé à accéder à la caméra.

--- app/src/main/java/fr/epsi/projetexamenv2/QrScanActivity.kt
+++ app/src/main/java/fr/epsi/projetexamenv2/QrScanActivity.kt
@@ -1,11 +1,25 @@
 package fr.epsi.projetexamenv2
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.budiyev.android.codescanner.CodeScanner
 
 class QrScanActivity : AppCompatActivity() {
+
+    private lateinit var codescanner: CodeScanner
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_qr_scan)
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 123)
+        } else {
+            
+        }
     }
 }
