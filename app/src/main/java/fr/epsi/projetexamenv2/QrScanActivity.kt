3dc7a73ea5afcb608dcab34d317f0a9bd97ce2a7
François Saura <franois.saura@gmail.com>
2022-03-02 21:08:44 +0100

    Ajout du code permettant de lire le QR Code et d'afficher son contenu string dans un toast.

--- app/src/main/java/fr/epsi/projetexamenv2/QrScanActivity.kt
+++ app/src/main/java/fr/epsi/projetexamenv2/QrScanActivity.kt
@@ -4,9 +4,15 @@ import android.Manifest
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.budiyev.android.codescanner.AutoFocusMode
 import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.CodeScannerView
+import com.budiyev.android.codescanner.DecodeCallback
+import com.budiyev.android.codescanner.ErrorCallback
+import com.budiyev.android.codescanner.ScanMode
 
 class QrScanActivity : AppCompatActivity() {
 
@@ -19,7 +25,46 @@ class QrScanActivity : AppCompatActivity() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 123)
         } else {
-            
+            startScanning()
+        }
+    }
+
+    private fun startScanning() {
+        val scannerView: CodeScannerView = findViewById(R.id.scanner_view)
+        codescanner = CodeScanner(this, scannerView)
+        codescanner.camera = CodeScanner.CAMERA_BACK
+        codescanner.formats = CodeScanner.ALL_FORMATS
+        codescanner.autoFocusMode = AutoFocusMode.SAFE
+        codescanner.scanMode = ScanMode.SINGLE
+        codescanner.isAutoFocusEnabled = true
+        codescanner.isFlashEnabled = false
+
+        codescanner.decodeCallback = DecodeCallback {
+            runOnUiThread {
+                Toast.makeText(this, it.text, Toast.LENGTH_LONG).show()
+            }
+        }
+
+        codescanner.errorCallback = ErrorCallback {
+            runOnUiThread {
+                Toast.makeText(this, "Erreur de lecture du code QR !", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        scannerView.setOnClickListener {
+            codescanner.startPreview()
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 123) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startScanning()
+            } else {
+                Toast.makeText(this, "Autorisation de la camera non accorde. Application ne va pas bien fonctionner.", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
