1e3604fb47e1ead9383324ec9b30d24482903516
TitouanDev <titouanescorneboueu@gmail.com>
2022-03-02 14:43:55 +0100

    Add BaseActivity, layout_header

---
+++ app/src/main/java/fr/epsi/projetexamenv2/BaseActivity.kt
@@ -0,0 +1,21 @@
+package fr.epsi.projetexamenv2
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+open class BaseActivity: AppCompatActivity() {
+    fun showButtonBacjk() {
+        val imageButtonBack = findViewById<ImageView>(R.id.imageViewBack)
+        imageButtonBack.visibility = View.VISIBLE
+        imageButtonBack.setOnClickListener(View.OnClickListener {
+            finish()
+        })
+    }
+
+    fun setStringHeader(txt: String) {
+        val textStrinHeader = findViewById<TextView>(R.id.textViewHeader)
+        textStrinHeader.text = txt
+    }
+}
--- app/src/main/java/fr/epsi/projetexamenv2/SplashActivity.kt
+++ app/src/main/java/fr/epsi/projetexamenv2/SplashActivity.kt
@@ -3,7 +3,7 @@ package fr.epsi.projetexamenv2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
