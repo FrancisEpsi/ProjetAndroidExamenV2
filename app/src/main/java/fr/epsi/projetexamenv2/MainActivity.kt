fd1ab9443d285987a031060c6de3bde3567d323e
TitouanDev <titouanescorneboueu@gmail.com>
2022-03-03 22:31:37 +0100

    Add ProductAdapter, ProductActivity, NavigationBar, self_product, Products

--- app/src/main/java/fr/epsi/projetexamenv2/MainActivity.kt
+++ app/src/main/java/fr/epsi/projetexamenv2/MainActivity.kt
@@ -1,11 +1,28 @@
 package fr.epsi.projetexamenv2
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.navigationView)
+        bottomNavigation.setOnNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.offres -> {
+                    val newIntent = Intent(application, OffresActivity::class.java)
+                    startActivity(newIntent)
+                    return@setOnNavigationItemSelectedListener true
+                }
+                else -> {
+                    Log.e("ERR", "Gros con")
+                    return@setOnNavigationItemSelectedListener false
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/fr/epsi/projetexamenv2/OffresActivity.kt
@@ -0,0 +1,10 @@
+package fr.epsi.projetexamenv2
+
+import android.os.Bundle
+
+class OffresActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_offres)
+    }
+}
