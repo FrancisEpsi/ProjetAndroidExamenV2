8ae3672f6c085b205809c0c1db4ff9b9092bd4cc
François Saura <franois.saura@gmail.com>
2022-03-03 14:32:30 +0100

    [TEMPORAIRE] Création d'un bouton sur le splash pour ouvrir la page QrScanActivity en vu de la tester

--- app/src/main/java/fr/epsi/projetexamenv2/SplashActivity.kt
+++ app/src/main/java/fr/epsi/projetexamenv2/SplashActivity.kt
@@ -1,11 +1,19 @@
 package fr.epsi.projetexamenv2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SplashActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val btnQr : Button = findViewById(R.id.button1)
+        btnQr.setOnClickListener {
+            val newIntent = Intent(application, QrScanActivity::class.java)
+            startActivity(newIntent)
+        }
     }
 }
