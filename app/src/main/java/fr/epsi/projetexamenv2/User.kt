5293da519c888119f0426cbe68cc4eaa93347863
François Saura <franois.saura@gmail.com>
2022-03-03 14:33:02 +0100

    Création d'une classe de compte d'utilisateur afin d'y stocker ses propriétées.

---
+++ app/src/main/java/fr/epsi/projetexamenv2/User.kt
@@ -0,0 +1,5 @@
+package fr.epsi.projetexamenv2
+
+public class User (val firstName: String, val lastName: String, val email: String, val address: String, val zipcode: String, val city: String, val cardRef: String){
+
+}
